f1e3507568955d31083d893034e1749ce75ff598
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-08-14 17:16:56 -0500

    Se realiza más pruebas en Test1.java

--- src/Pruebas/Test1.java
+++ src/Pruebas/Test1.java
@@ -1,4 +1,8 @@
-public class Test1 {
+package Pruebas;
+
+import HashTable.Hash;
+
+public class Test11111111111 {
 	public static void main(String[] args) {
 		Hash nombres = new Hash(25);
 		
@@ -8,6 +12,13 @@ public class Test1 {
 		nombres.put(96, "Osmar Gonzales");
 		
 		System.out.println(nombres);
-		
+		System.out.println("Pruebas del método Get");
+		System.out.println(nombres.get(25));
+		System.out.println("Pruebas del método Remove");
+		System.out.println(nombres.remove(25));
+		System.out.println(nombres.get(25));
+		System.out.println("Pruebas del método Put en la misma clave");
+		System.out.println(nombres.put(34,"Luis Angel"));
+		System.out.println(nombres);
 	}
 }
