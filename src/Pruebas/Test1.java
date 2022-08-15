5500224cd625852f55982d60d8167e024dbe3ede
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-08-14 20:55:27 -0500

    Se hacen correciones al Test1.java y se ajusta su ejecucción.

--- src/Pruebas/Test1.java
+++ src/Pruebas/Test1.java
@@ -2,7 +2,7 @@ package Pruebas;
 
 import HashTable.Hash;
 
-public class Test {
+public class Test1 {
 	public static void main(String[] args) {
 		Hash nombres = new Hash(25);
 		
@@ -11,20 +11,14 @@ public class Test {
 		nombres.put(15, "Deiman Galvez");
 		nombres.put(96, "Osmar Gonzales");
 
-		nombres.put(96, "Carlos Gonzales");
 		System.out.println(nombres);
 		nombres.put(115, "Charo Gonzales");
-		System.out.println(nombres);
-		nombres.put(215, "Felipe Gonzales");
-		System.out.println(nombres);
-		nombres.put(315, "María Gonzales");
 		System.out.println(nombres);
 		nombres.put(315, "Jorge Gonzales");
 		System.out.println(nombres);
 		nombres.put(96, "Marco Gonzales");
 		System.out.println(nombres);
 		
-		System.out.println(nombres.remove(215));
-		System.out.println(nombres);
+		
 	}
 }
